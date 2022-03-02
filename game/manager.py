22b16d133dfcb51521fa5bf9503d19afdae19729
JoshHughes156 <joshhughes2005@yahoo.co.uk>
2022-03-02 14:24:30 +0000

    Added action choice menu Able to travel between neighbouring locations Modified gitignore to ignore .swp files

--- game/manager.py
+++ game/manager.py
@@ -22,5 +22,27 @@ player = Entity(name, 10, town_square)
 while True:
 
     # Get data about current location
-    print(f"You are currently in {player.location.name}")
+    print(f"\nYou are currently in {player.location.name}")
 
+    print("\nWhat would you like to do?:\n\
+           1) View nearby locations\n\
+           2) View nearby NPCs\n\
+           3) Exit game")
+
+    choice = input("Please enter your choice: ")
+    while choice not in ["1", "2", "3"]:
+        print("Please enter a valid choice")
+        choice = input("Please enter your choice: ")
+
+    if choice == "1":
+        for i, n in enumerate(player.location.get_neighbours()):
+            print(f"{i+1}: {n.name}")
+        l = input("Would you like to travel to any of these locations (0 to remain in current location): ")
+        if int(l) <= len(player.location.get_neighbours()) and int(l) > 0:
+            player.location = player.location.get_neighbours()[i-1]
+            continue
+    elif choice == "2":
+        continue
+    elif choice == "3":
+        print("\nBYE\n")
+        break
